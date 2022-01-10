50056fea281e045121ecc3674c40ab38d8767c74
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-01-10 12:22:13 +0000

    [Update]members_controller

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,6 +1,7 @@
 class Public::MembersController < ApplicationController
 
   def show
+    @member = current_member
   end
 
   def edit
@@ -13,21 +14,31 @@ class Public::MembersController < ApplicationController
     redirect_to root_path
   end
 
+  #退会確認画面
   def confirm
   end
 
+  #退会用アクション(PATCH(=論理削除))
   def withdraw
+    @member = current_member
+    # is_deletedカラムをtrue(=退会)にupdateする
+    @member.update(is_deleted: true)
+    # session(ページ遷移しても以前入力した情報を保持する機能)をリセットし、ログアウト
+    reset_session
+    redirect_to root_path
   end
 
+  #フォロー一覧画面(GET)
   def follows
   end
 
+  #フォロワー一覧画面(GET)
   def followers
   end
 
   private
   def member_params
     params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :user_name,
-      :gender, :birthday, :email, :living_prefecture, :favorite_word, :profile_image, :background_image )
+      :gender, :birthday, :email, :living_prefecture, :favorite_word, :profile_image, :background_image, :is_ )
   end
 end
