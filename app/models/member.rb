504b926d1e112e45dcd0cf8ad8f73653c337c2d3
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-01-12 03:11:15 +0000

    [Update]enum prefecture

--- app/models/member.rb
+++ app/models/member.rb
@@ -57,15 +57,23 @@ class Member < ApplicationRecord
     Male:1, Female:2, Secret:3
   }
 
-  #enum 都道府県
+  #enum 会員登録用(都道府県)
   enum living_prefecture: {
+    Secret:0,
+    #北海道・東北地方
     Hokkaido:1, Aomori:2, Iwate:3, Miyagi:4, Akita:5, Yamagata:6, Fukushima:7,
+    #関東地方
     Ibaraki:8, Tochigi:9, Gunma:10, Saitama:11, Chiba:12, Tokyo:13, Kanagawa:14,
+    #中部地方
     Niigata:15, Toyama:16, Ishikawa:17, Fukui:18, Yamanashi:19, Nagano:20,
     Gifu:21, Shizuoka:22, Aichi:23, Mie:24,
+    #近畿地方
     Shiga:25, Kyoto:26, Osaka:27, Hyogo:28, Nara:29, Wakayama:30,
+    #中国地方
     Tottori:31, Shimane:32, Okayama:33, Hiroshima:34, Yamaguchi:35,
+    #四国地方
     Tokushima:36, Kagawa:37, Ehime:38, Kochi:39,
+    #九州・沖縄
     Fukuoka:40, Saga:41, Nagasaki:42, Kumamoto:43, Oita:44, Miyazaki:45, Kagoshima:46,
     Okinawa:47
   }
