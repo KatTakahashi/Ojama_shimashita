00c405d0fff976959520f959a7159f401d4accfb
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-02-02 03:17:10 +0000

    [Update]layout

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,12 +3,10 @@ class Public::RelationshipsController < ApplicationController
 # --------------- フォロー機能 --------------
  def follow
   current_member.follows(params[:id])
-  redirect_to member_path(params[:id])
  end
 
 # --------------- フォロー解除機能 --------------
  def unfollow
   current_member.unfollow(params[:id])
-  redirect_to member_path(params[:id])
  end
 end
