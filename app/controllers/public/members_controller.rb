6e6d80645405812b8e1366c143b2bc1e95ecc253
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-02-12 02:10:19 +0000

    [Update]img_scroll_page_per

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -4,7 +4,7 @@ class Public::MembersController < ApplicationController
   def show
     @member = Member.find(params[:id])
     @posts = Post.where(member_id: params[:id]).order(taken_at: :desc)
-    @posts_scroll = Kaminari.paginate_array(@posts).page(params[:page]).per(1)
+    @posts_scroll = Kaminari.paginate_array(@posts).page(params[:page]).per(5)
     @posts_count = Post.where(member_id: @member.id)
     @visiteds = Post.where(member_id: @member.id).select(:prefecture).distinct.order('prefecture').map { |i| Post.prefectures[i.prefecture] }
     gon.visiteds = @visiteds
